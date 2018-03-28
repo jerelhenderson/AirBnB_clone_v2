539661171515a7ec02c6a9aa6f0657d93113288f
Daniel Ojeda <211@holbertonschool.com>
2018-03-28 05:22:50 +0000

    fix indent

--- models/user.py
+++ models/user.py
@@ -11,10 +11,10 @@ class User(BaseModel, Base):
     '''
         Definition of the User class
     '''
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128))
-        last_name = Column(String(128))
-        places = relationship("Place",
-                              backref="user", cascade="all, delete")
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place",
+                          backref="user", cascade="all, delete")
