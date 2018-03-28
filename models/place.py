47812094ab1f616c45e3e151a2c4b8d622e5c26c
Daniel Ojeda <211@holbertonschool.com>
2018-03-28 23:25:29 +0000

    fix syntax

--- models/place.py
+++ models/place.py
@@ -4,7 +4,7 @@
 '''
 from models.base_model import BaseModel, Base
 import models
-import os
+from os import environ
 from sqlalchemy import Column, String, Integer, ForeignKey, DateTime, Float
 from sqlalchemy.orm import relationship
 
@@ -13,9 +13,9 @@ class Place(BaseModel, Base):
     '''
         Define the class Place that inherits from BaseModel.
     '''
-    __tablename__ = 'place'
-    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    __tablename__ = "places"
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
