422515e6e2b89d954a62da3cf314007ecb8cbf5f
Daniel Ojeda <211@holbertonschool.com>
2018-03-28 05:56:04 +0000

    back

--- models/state.py
+++ models/state.py
@@ -3,28 +3,11 @@
     Implementation of the State class
 '''
 
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models.city import City
-from os import environ
-import models
+from models.base_model import BaseModel
 
 
-class State(BaseModel, Base):
-    '''
-        Implementation for the State.
-    '''
-    if environ.get("HBNB_TYPE_STORAGE") == "db":
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", cascade="delete")
-    else:
-        @property
-        def cities(self):
-            cityl = []
-            allc = models.storage.all(City)
-            for key, value in allc.items():
-                if value.state_id == self.id:
-                    cityl.append(value)
-            return cityl
+class State(BaseModel):
+    """
+    Implementation for the State.
+    """
+    name = ""
