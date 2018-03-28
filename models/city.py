70969a1b69a7e113fbb4b32c4ab5eb944cae8ee0
Daniel Ojeda <211@holbertonschool.com>
2018-03-28 21:32:35 +0000

    changaes for 8

--- models/city.py
+++ models/city.py
@@ -16,5 +16,4 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        places = relationship("Place", backref="cities", cascade="delete")
+    places = relationship("Place", backref="cities")
