8bf3e9be63157a38593263d9e8cc4b8e81ddcacc
Jerel Henderson <henderson.jerel@yahoo.com>
2018-04-23 06:55:53 +0000

    Desperation push

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -82,3 +82,9 @@ class DBStorage():
                                        expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+    def close(self):
+        """
+        close session
+        """
+        self.__session.remove()
