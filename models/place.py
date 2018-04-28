8807a160bd58c2b50bd469978d70970c043b788d
Jerel Henderson <henderson.jerel@yahoo.com>
2018-04-28 17:11:34 +0000

    Add small changes to models

--- models/place.py
+++ models/place.py
@@ -5,7 +5,7 @@
 from models.base_model import BaseModel, Base
 import models
 from os import environ
-from sqlalchemy import Column, String, Integer, ForeignKey, DateTime, Float
+from sqlalchemy import Column, String, Integer, ForeignKey, Float, Table
 from sqlalchemy.orm import relationship
 
 
@@ -24,4 +24,3 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    reviews = relationship("Review", backref="place", cascade="delete")
