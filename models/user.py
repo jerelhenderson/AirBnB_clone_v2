f2371c03035d83adfd209098802b01fb7eae09c3
Daniel Ojeda <211@holbertonschool.com>
2020-03-27 16:53:45 +0000

    update

--- models/user.py
+++ models/user.py
@@ -3,13 +3,25 @@
     Implementation of the User class which inherits from BaseModel
 '''
 from models.base_model import BaseModel
+from os import environ
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel):
     '''
         Definition of the User class
     '''
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    if environ.get('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+        places = relationship("Place",
+                              backref="user", cascade="all, delete")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
