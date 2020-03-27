0b526cc2dc6ef1fb78742703af6fca2eb880e39d
Daniel Ojeda <211@holbertonschool.com>
2020-03-27 17:37:26 +0000

    user

--- models/user.py
+++ models/user.py
@@ -2,19 +2,14 @@
 '''
     Implementation of the User class which inherits from BaseModel
 '''
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
 
 
-class User(BaseModel, Base):
+class User(BaseModel):
     '''
         Definition of the User class
     '''
-    __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place",
-                          backref="user", cascade="all, delete")
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
