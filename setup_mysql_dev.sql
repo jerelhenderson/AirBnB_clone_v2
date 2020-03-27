d764df4d4c4149f2974523c63d40df9de37352d4
Daniel Ojeda <211@holbertonschool.com>
2020-03-27 05:44:23 +0000

    Prepares a MySQL server for project

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,5 @@
+-- This is a MySQL script that prepares a MySQL server
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbhb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
