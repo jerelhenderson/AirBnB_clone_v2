62efa9937560e9c7207c08d07811d0a922ed3e8d
Daniel Ojeda <211@holbertonschool.com>
2020-03-27 14:34:18 +0000

    sqlalchemy

--- models/city.py
+++ models/city.py
@@ -2,12 +2,16 @@
 '''
     Define the class City.
 '''
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, ForeignKey
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     '''
         Define the class City that inherits from BaseModel.
     '''
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+
