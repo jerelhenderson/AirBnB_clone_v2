8c378fab6c6f298db66d2f7271cc44a88b39ab2c
Daniel Ojeda <211@holbertonschool.com>
2020-03-28 05:21:26 +0000

    city changes

--- models/city.py
+++ models/city.py
@@ -2,12 +2,19 @@
 '''
     Define the class City.
 '''
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+import models
+import os
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     '''
         Define the class City that inherits from BaseModel.
     '''
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        places = relationship("Place", backref="cities", cascade="delete")
