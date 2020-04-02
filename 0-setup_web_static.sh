d6f77184bfb74d63ba4a2660195fef0ed38eb5f9
Jerel Henderson <henderson.jerel@yahoo.com>
2020-04-02 18:35:24 +0000

    Initial Commit - AirBNB_v2, Apr. 2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
-# Create server configuration
-sudo apt-get -y update
-sudo apt-get -y upgrade
+# sets up web servers for deployment of web_static
+sudo apt-get update
 sudo apt-get -y install nginx
+
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-sudo touch /data/web_static/releases/test/index.html
-echo "Simple Content" | sudo tee /data/web_static/releases/test/index.html
+sudo echo -e "This is a test" >> /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '38i\ \tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
+sudo chown -R ubuntu:ubuntu /data
+
+static="\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n}"
+sudo sed -i "38i\ $static" /etc/nginx/sites-available/default
+
 sudo service nginx restart
---
+++ 3-deploy_web_static.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+Fabric script generates .tgz archive from contents of web_static directory
+"""
+from fabric.api import local
+from datetime import datetime
+
+
+env.hosts = ['54.152.200.18', '54.224.57.104']
+
+
+def do_pack():
+    """ return archive path if successful """
+    cur_time = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    local("mkdir -p versions")
+    try:
+        local("tar -cvzf versions/web_static_{}.tgz web_static".format(
+            cur_time))
+        return ("versions/web_static_{}.tgz".format(cur_time))
+    except:
+        return None
+
+import os
+
+
+def do_deploy(archive_path):
+    """ return `True` if successful """
+
+    if os.path.exists(archive_path):
+        return None
+    else:
+        return False
+
+    try:
+        put(archive_path, "/tmp"))
+        run("mkdir -p /data/web_static/releases/{}".format())
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format())
+        run("rm /tmp/{}".format())
+        run("mv /data/web/static/releases/{}".format())
+        run("rm -rf /data/web_static/relases/{}".format())
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases{}".format())
+        return True
+    except:
+        return False
+
+def deplay():
+    """ returns value of do_deplay """
+    archive = do_pack()
+    if archive is True:
+        return do_deplay(archive)
+    else:
+        return False
