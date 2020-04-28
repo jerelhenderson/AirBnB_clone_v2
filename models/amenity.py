53990b4822e2a27e2294c5b4854f00e4d88253cf
Jerel Henderson <henderson.jerel@yahoo.com>
2020-04-28 04:49:06 +0000

    Add small changes to models

--- models/amenity.py
+++ models/amenity.py
@@ -3,10 +3,14 @@
     Implementation of the Amenity class
 '''
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import environ
 
 
 class Amenity(BaseModel):
     '''
         Implementation for the Amenities.
     '''
-    name = ""
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
