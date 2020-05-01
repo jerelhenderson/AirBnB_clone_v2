7c603cd783d8b6f9aa43ed18517b15ce6b406d13
Jerel Henderson <henderson.jerel@yahoo.com>
2020-05-01 10:04:02 +0000

    Fix Task 10

--- models/base_model.py
+++ models/base_model.py
@@ -24,18 +24,19 @@ class BaseModel:
         '''
             Initialize public instance attributes.
         '''
-        if (len(kwargs) == 0):
+        if "id" not in kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+
         else:
             kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
                                                      "%Y-%m-%dT%H:%M:%S.%f")
             kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
                                                      "%Y-%m-%dT%H:%M:%S.%f")
-            for key, val in kwargs.items():
-                if "__class__" not in key:
-                    setattr(self, key, val)
+        for key, val in kwargs.items():
+            if "__class__" not in key:
+                setattr(self, key, val)
 
     def __str__(self):
         '''
