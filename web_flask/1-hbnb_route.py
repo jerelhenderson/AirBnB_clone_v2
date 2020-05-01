e5e3331693e9a7aecaeece445b5739aa6331d563
Jerel Henderson <henderson.jerel@yahoo.com>
2020-05-01 01:33:16 +0000

    Fix typo

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -17,7 +17,7 @@ def hello_flask():
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ display 'HBNB' """
-    return "HBNB!"
+    return "HBNB"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
